d20552ea93488dc3275d0cdd8bd63a4a76b3f7cc
Lirys <iamyourmarkieboo@gmail.com>
2023-12-02 12:33:11 +0800

    Unfinished vehicle query on joining

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
 
 class VehicleController extends Controller
 {
-    //
+    public function fetchAll(): View {
+        $merged = DB::table('cars')->beforeQuery(function ($column){
+
+        })
+            ->union(DB::table('motorcycles'))
+            ->get();
+        return view('vehicles.display', ['data' => $merged]);
+    }
 }
---
+++ resources/views/vehicles/display.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('/css/generic-table-style.css')}}">
+    <title>Vehicles</title>
+</head>
+<body>
+
+    <h1>Vehicles</h1>
+    <button onclick="window.location.href='{{ route('vehicles.motorcycles.create')}}'">Add</button>
+
+    @foreach($data as $element)
+        @dd($element)
+    @endforeach
+
+    <div class="container-table">
+        <table>
+            <thead>
+            <tr>
+                <th>Plate Number</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Color</th>
+                <th>Availability</th>
+                <th>Owner</th>
+                <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($data as $element)
+                @dd($element)
+                <tr>
+                    @foreach($element->attributesToArray() as $displayable)
+                        <td>{{$displayable}}</td>
+                    @endforeach
+                    <td>
+                        <form method="POST">
+                            <button type="submit" formmethod="get" formaction=
+                                "/car/edit/{{$element->getAttribute($element->getKeyName())}}">
+                                Edit</button>
+                            <button type="submit" formmethod="get" formaction=
+                                "/car/delete/{{$element->getAttribute($element->getKeyName())}}">
+                                Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+
+</body>
+</html>
