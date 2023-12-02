6e5249f178306adec1b2a3317eb5ca229ab2a118
Lirys <iamyourmarkieboo@gmail.com>
2023-12-02 12:37:41 +0800

    Leftover uncommitted Request object

---
+++ app/Http/Requests/CarMotorRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CarMotorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'plateNo' => 'required',
+            'name' => 'required',
+            'type' => 'required',
+            'color' => 'required',
+            'availability' => 'required|boolean',
+            'leaser' => 'required|exists:leasers,leaser_name'
+        ];
+    }
+}
